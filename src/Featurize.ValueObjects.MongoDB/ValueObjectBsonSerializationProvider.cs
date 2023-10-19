d91e30602d85bcf41a67a2fbb784b22d2681ed74
Patrick Evers <pmdevers@gmail.com>
2023-10-19 13:00:48 +0200

    Add ValueObjetct Bson Serialization Provider

---
+++ src/Featurize.ValueObjects.MongoDB/ValueObjectBsonSerializationProvider.cs
@@ -0,0 +1,22 @@
+﻿using Featurize.ValueObjects.Interfaces;
+using MongoDB.Bson.Serialization;
+
+namespace Featurize.ValueObjects;
+
+/// <summary>
+/// Serialization Provider for serializing <see cref="IValueObject{T}"/> to Bson.
+/// </summary>
+public sealed class ValueObjectBsonSerializationProvider : IBsonSerializationProvider
+{
+    /// <inheritdoc />
+    public IBsonSerializer GetSerializer(Type type)
+    {
+        if (type.GetInterfaces().Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IValueObject<>)))
+        {
+            var serializerType = typeof(ValueObjectBsonSerializer<>).MakeGenericType(type);
+            return (IBsonSerializer)Activator.CreateInstance(serializerType)!;
+        }
+
+        return null!;
+    }
+}
---
+++ src/Featurize.ValueObjects.MongoDB/ValueObjectBsonSerializer.cs
@@ -0,0 +1,33 @@
+﻿using Featurize.ValueObjects.Interfaces;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson.Serialization;
+using System.Globalization;
+
+namespace Featurize.ValueObjects;
+
+/// <summary>
+/// Serializer for serializing <see cref="IValueObject{T}"/> to Bson.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public sealed class ValueObjectBsonSerializer<T> : SerializerBase<T>, IBsonSerializer
+    where T : IValueObject<T>
+{
+
+    /// <inheritdoc />
+    public override T Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+    {
+        try
+        {
+            var value = context.Reader.ReadString()!;
+            return T.TryParse(value, CultureInfo.InvariantCulture, out var result) ? result : T.Unknown;
+        }
+        catch
+        {
+            return T.Unknown;
+        }
+    }
+
+    /// <inheritdoc />
+    public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, T value)
+        => context.Writer.WriteString(value.ToString());
+}
