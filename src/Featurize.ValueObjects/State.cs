4c189a15bad4ccc3b940e5a65c0bdfa412ac27df
Patrick Evers <pmdevers@gmail.com>
2023-11-01 15:30:22 +0100

    State Object

---
+++ src/Featurize.ValueObjects/State.cs
@@ -0,0 +1,81 @@
+﻿using Featurize.ValueObjects.Interfaces;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Featurize.ValueObjects;
+
+/// <summary>
+/// Simple State Machine
+/// </summary>
+public record State
+{
+    private State(int Value) { Ordinal = Value; }
+
+    private HashSet<int> _transitions = new();
+    
+    /// <summary>
+    /// The name of this state.
+    /// </summary>
+    public string Name { get; private set; } = string.Empty;
+
+    /// <summary>
+    /// The Ordinal value
+    /// </summary>
+    public int Ordinal { get; private set; }
+
+    /// <summary>
+    /// Create a <see cref="State"/> for a specific ordinal.
+    /// </summary>
+    /// <param name="ordinal">The ordinal for this state.</param>
+    /// <returns>Returns a state of the ordinal.</returns>
+    public static State Create(int ordinal) => new(ordinal);
+
+    /// <summary>
+    /// Gives <see cref="State"/> a name.
+    /// </summary>
+    /// <param name="name">The name of this state.</param>
+    /// <returns>Returns the <see cref="State"/> with the given name.</returns>
+    public State WithName(string name) => this with { Name = name };
+
+    /// <summary>
+    /// Sets the allowed Transitions for this <see cref="State"/>.
+    /// </summary>
+    /// <param name="allowedTransitions">The ordinals of the allowed transitions.</param>
+    /// <returns>Returns the <see cref="State"/> with the allowed transitions.</returns>
+    public State WithAllowedTransitions(params int[] allowedTransitions)
+    {
+        var hset = new HashSet<int>();
+        foreach (var transitions in allowedTransitions)
+        {
+            hset.Add(transitions);
+        }
+        return this with { _transitions = hset };
+    }
+
+    /// <summary>
+    /// Indicates if a this state can transition to the new state.
+    /// </summary>
+    /// <param name="newState">The <see cref="State"/> where to transition.</param>
+    /// <returns>Returns <c>true</c> if the Transition is allowed.</returns>
+    public bool CanTransitionTo(State newState)
+        => _transitions.Contains(newState.Ordinal);
+
+    /// <summary>
+    /// Transition to the new state.
+    /// </summary>
+    /// <param name="newState">The new state.</param>
+    /// <returns>Returns the new state if the transition is allowed.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when the transition is not allowed.</exception>
+    public State TransitionTo(State newState)
+    {
+        if(!_transitions.Contains(newState.Ordinal))
+           throw new InvalidOperationException($"Transition to: {newState.Ordinal} not allowed.");
+
+        return newState;
+    }
+    
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return Name;
+    }
+}
---
+++ test/Featurize.ValueObjects.Tests/State_Specs.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+
+namespace Featurize.ValueObjects.Tests;
+internal class State_Specs
+{
+    [Test]
+    public void CanTransitionTo()
+    {
+        var start = State.Create(0)
+            .WithName("Start")
+            .WithAllowedTransitions(1);
+
+        var inprocess = State.Create(1)
+            .WithName("InProcess")
+            .WithAllowedTransitions(2);
+
+        var end = State.Create(2)
+            .WithName("End")
+            .WithAllowedTransitions(0);
+        
+        start.CanTransitionTo(inprocess).Should().BeTrue();
+        start.CanTransitionTo(end).Should().BeFalse();
+
+        inprocess.CanTransitionTo(end).Should().BeTrue();
+        inprocess.CanTransitionTo(start).Should().BeFalse();
+
+        end.CanTransitionTo(start).Should().BeTrue();
+        end.CanTransitionTo(inprocess).Should().BeFalse();
+    }
+
+    [Test]
+    public void Transition()
+    {
+        var current = StateMachine.Start;
+
+        current = current.TransitionTo(StateMachine.InProcess);
+
+        current.Should().Be(StateMachine.InProcess);
+
+        current = current.TransitionTo(StateMachine.End);
+
+        current.Should().Be(StateMachine.End);
+    }
+}
+
+public static class StateMachine
+{
+    public static State Start = State.Create(0)
+        .WithName(nameof(Start))
+        .WithAllowedTransitions(1);
+
+    public static State InProcess = State.Create(1)
+        .WithName(nameof(InProcess))
+        .WithAllowedTransitions(2);
+
+    public static State End = State.Create(2)
+        .WithName(nameof(End))
+        .WithAllowedTransitions(0);
+}
