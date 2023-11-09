6749261204d0fadac5d0d717dbb1b29fa253c0dc
Patrick Evers <pmdevers@gmail.com>
2023-11-09 22:14:54 +0100

    Implement Roman numeral

---
+++ src/Featurize.ValueObjects/RomanNumeral.cs
@@ -0,0 +1,152 @@
+﻿using Featurize.ValueObjects.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Featurize.ValueObjects;
+public record struct RomanNumeral() : IValueObject<RomanNumeral>
+{
+    private const string _unknownvalue = "?";
+    private int? _value = 0;
+
+    private static Dictionary<char, int> _values = new()
+    {
+        { 'I', 1 },
+        { 'V', 5 },
+        { 'X', 10 },
+        { 'L', 50 },
+        { 'C', 100 },
+        { 'D', 500 },
+        { 'M', 1000 },
+    };
+
+    public static RomanNumeral Unknown => new() {  _value = null };
+
+    public static RomanNumeral Empty => new();
+
+    public override string ToString()
+    {
+        StringBuilder result = new();
+        var num = _value ?? 0;
+
+        if (num >= 1000)
+        {
+            result.Append(new string('M', num / 1000));
+            num %= 1000;
+        }
+        if (num >= 100)
+        {
+            result.Append(num / 900 >= 1 ? "CM" :
+                            num / 500 >= 1 ? $"D{new string('C', num / 100 - 5)}" :
+                            num / 400 >= 1 ? "CD" : new string('C', num / 100));
+            num %= 100;
+        }
+
+        if (num >= 10)
+        {
+            result.Append(num / 90 >= 1 ? "XC" :
+                            num / 50 >= 1 ? $"L{new string('X', num / 10 - 5)}" :
+                            num / 40 >= 1 ? "XL" : new string('X', num / 10));
+            num %= 10;
+        }
+
+        result.Append(num / 9 >= 1 ? "IX" :
+                        num / 5 >= 1 ? $"V{new string('I', num - 5)}" :
+                        num / 4 >= 1 ? "IV" : new string('I', num));
+
+        return result.ToString();
+    }
+
+    public static RomanNumeral Parse(string s)
+        => Parse(s, null);
+
+    public static RomanNumeral Parse(string s, IFormatProvider? provider)
+        => TryParse(s, out var result) ? result : throw new FormatException();
+
+    public static bool TryParse([NotNullWhen(true)] string? s, [MaybeNullWhen(false)] out RomanNumeral result)
+        => TryParse(s, null, out result);
+
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out RomanNumeral result)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            result = Empty;
+            return true;
+        }
+
+        if(s == _unknownvalue)
+        {
+            result = Unknown;
+            return true;
+        }
+
+        var total = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (i < s.Length - 1 && _values[s[i]] < _values[s[i + 1]])
+            {
+                total -= _values[s[i]];
+            }
+            else
+            {
+                total += _values[s[i]];
+            }
+        }
+
+        result = new() { _value = total };
+        return true;
+    }
+
+    public bool IsEmpty() 
+        => this == Empty;
+
+    public static bool operator ==(RomanNumeral a, int b)
+    {
+        return a._value == b;
+    }
+
+    public static bool operator !=(RomanNumeral a, int b)
+    {
+        return !(a == b);
+    }
+
+    public static RomanNumeral operator +(RomanNumeral a, RomanNumeral b)
+        => new() { _value = a._value + b._value };
+    public static RomanNumeral operator +(RomanNumeral a, int b)
+        => new() { _value = a._value + b };
+
+    public static RomanNumeral operator -(RomanNumeral a, RomanNumeral b)
+        => new() { _value = a._value - b._value };
+    public static RomanNumeral operator -(RomanNumeral a, int b)
+        => new() { _value = a._value - b };
+    public static RomanNumeral operator /(RomanNumeral a, RomanNumeral b)
+            => new() { _value = a._value / b._value };
+    public static RomanNumeral operator /(RomanNumeral a, int b)
+        => new() { _value = a._value / b };
+    public static RomanNumeral operator *(RomanNumeral a, RomanNumeral b)
+            => new() { _value = a._value * b._value };
+    public static RomanNumeral operator *(RomanNumeral a, int b)
+        => new() { _value = a._value * b };
+
+   
+    public static RomanNumeral operator ++(RomanNumeral a)
+        => new() {  _value = a._value++ };
+
+    public static RomanNumeral operator --(RomanNumeral a)
+        => new() { _value = a._value-- };
+
+    public static implicit operator int(RomanNumeral a)
+        => a._value ?? 0;
+
+    public static explicit operator RomanNumeral(int a)
+        => new () { _value = a };
+
+    public static implicit operator string(RomanNumeral a)
+        => a.ToString();
+
+    public static implicit operator RomanNumeral(string a)
+        => Parse(a);
+}
---
+++ test/Featurize.ValueObjects.Tests/RomanNumeral_Specs.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+
+namespace Featurize.ValueObjects.Tests;
+internal class RomanNumeral_Specs
+{
+    [TestCase("I", 1)]
+    [TestCase("II", 2)]
+    [TestCase("III", 3)]
+    [TestCase("IV", 4)]
+    [TestCase("V", 5)]
+    [TestCase("VI", 6)]
+    [TestCase("VII", 7)]
+    [TestCase("VIII", 8)]
+    [TestCase("IX", 9)]
+    [TestCase("X", 10)]
+    [TestCase("XI", 11)]
+    [TestCase("XII", 12)]
+    [TestCase("XIII", 13)]
+    [TestCase("XIV", 14)]
+    [TestCase("XV", 15)]
+    [TestCase("L", 50)]
+    [TestCase("C", 100)]
+    [TestCase("D", 500)]
+    [TestCase("M", 1000)]
+    public void Should_be_valid_value(string str, int value)
+    {
+        var n = RomanNumeral.Parse(str);
+
+        (n == value).Should().BeTrue();
+    }
+
+
+    [TestCase(1994, "MCMXCIV")]
+    [TestCase(58, "LVIII")]
+    public void should_convertto_string(int value, string str)
+    { 
+        var a = RomanNumeral.Parse(str);
+        (a == value).Should().BeTrue();
+
+        RomanNumeral val = str;
+
+        (val == value).Should().BeTrue();
+
+        (val++ == value++).Should().BeTrue();
+        
+    }
+
+}
